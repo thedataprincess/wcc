d318c20545b9b131f02dda2adc26f882bb63f6ed
thedataprincess <oayoani@NY-DATA-005.local>
2017-04-18 14:13:41 -0400

    Booleans

---
+++ python/booleans.py
@@ -0,0 +1,22 @@
+#print(1 == 1) # expected output true
+
+#print(1 == 2) # Expected output: False
+
+#print(1 > 2) # Expected output:False
+
+#print(2 > 1) # Expected output:True
+
+#print(1 >= 1) # Expected output:True
+
+#print(2 == 2) # Expected output:True
+
+#print(2 != 2) # Expected output:False
+
+#gesture = 'rock'
+#print(gesture == 'rock' or gesture == 'paper' or gesture == 'scissors') # Expected outcome:
+
+#gesture = 'pape'
+#print(gesture == 'rock' or gesture == 'paper' or gesture == 'scissors') # Expected outcome:
+
+age = int(raw_input('How old are you?'))
+print(age > 5 and age < 10 and age != 5 and age != 10)
