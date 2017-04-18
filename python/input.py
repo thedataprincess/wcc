009273244c6943ae4ca251c9e95350a1fbf959a6
thedataprincess <oayoani@NY-DATA-005.local>
2017-04-18 13:31:44 -0400

    hell_world.py module

---
+++ python/input.py
@@ -0,0 +1,50 @@
+#name = raw_input( 'What is your name? ')
+#print('Hi ' + name)
+
+#name = raw_input( 'What is your name?' )
+#age = raw_input( 'How old are you?' )
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input( 'How old are you?' )
+#dog_years = int(age) * 7
+#print( 'You are ' + str(dog_years) + ' old in dog years.' )
+
+#cost_meal = float(raw_input( 'How much was your meal?' ))
+#tip = cost_meal * .20
+#print( 'You should tip $' + str(tip) )
+#print( 'Your total cost would be $' + str(total_cost))
+
+print( 'Welcome to the Mad Lib Generator!')
+
+
+#adjective1 = raw_input('Type in an adjective: ')
+#adjective2 = raw_input('Another adjective: ')
+#adjective3 = raw_input('And another adjective: ')
+#adjective4 = raw_input('One more adjective: ')
+#plural_animal = raw_input('Plural animal (e.g., puppies): ')
+#verb = raw_input('Now a verb: ')
+#plural_noun = raw_input('Now a plural noun: ')
+#woman_musician = raw_input('The first name of: ')
+#drink = raw_input('A beverage: ')
+#plural_body = raw_input('And finally, a plural body part: ')
+
+print('Excellent choices! Heres your story...')
+print('--------------------------------')
+print('There once was a' + adjective1 + 'programmer named ' + woman_musician)
+print('who wanted to build a' + adjective2 + 'mobile app to')
+print('help' + adjective3 + plural_animal + 'find new homes
+print('')
+print(woman_musician + 'stayed up all night, drinking lots of')
+print('caffeinated' + drink + 'as she worked and worked')
+print('trying to complete her'+ adjective4 + 'program')
+print('')
+print('Whenever' + woman_musician + 'hit a dead end, she would')
+print('exclaim *' + plural_noun + '*!')
+print('')
+print('But she was not discouraged, and after a quick break')
+print('to ' + verb + 'and clear her head, she got back to work.')
+print('')
+print('By morning, when the sun started to rise, she let out a')
+print('big yawn and stretched her' + plural_body)
+print('Finally, she was done.')
+print('--------------------------------')
---
+++ python/variables.py
@@ -0,0 +1,8 @@
+
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print("Ariel is " + str(age) + " years old now, but she will turn " + str(next_age) + " in " + birth_month + ".")
+print("Her birth stone is " + birth_stone + ".")
